3c4d2015d842b9b0496956908864ddd17d6b747e
Mohamed <mohammed.korany.ali@gmail.com>
2020-01-27 07:43:12 +0200

    Initial Commit

---
+++ DesignPatterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignPatterns
+//
+//  Created by Mohamed Korany Ali on 1/27/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
